5e1772126b9fa1bf00e30b0d087c2c74a8dafaa7
William.HU <william315@126.com>
2017-05-11 21:55:01 +0800

    commit test code

---
+++ src/main/java/com/shqs/sumilux/DN_William/Math.java
@@ -0,0 +1,27 @@
+package com.shqs.sumilux.DN_William;
+
+public class Math {
+
+	int a, b;
+
+	Math(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	public int add() {
+		return a + b;
+	}
+
+	public int minus() {
+		return a - b;
+	}
+
+	public int multiplied() {
+		return a * b;
+	}
+
+	public int divided() {
+		return a / b;
+	}
+}
---
+++ src/test/java/com/shqs/sumilux/DN_William/MathTest.java
@@ -0,0 +1,35 @@
+package com.shqs.sumilux.DN_William;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class MathTest {
+	Math math;
+
+	@Before
+	public void setUp() throws Exception {
+		math = new Math(10, 7);
+	}
+
+	@Test
+	public void testAdd() {
+		Assert.assertEquals(17, math.add());
+	}
+
+	@Test
+	public void testMinus() {
+		Assert.assertEquals(3, math.minus());
+	}
+
+	@Test
+	public void testMultiplied() {
+		Assert.assertEquals(70, math.multiplied());
+	}
+
+	@Test
+	public void testDivided() {
+		Assert.assertEquals(1, math.divided());
+	}
+}
